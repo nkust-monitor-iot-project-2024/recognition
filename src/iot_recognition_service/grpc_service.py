975ace594eed87a6ca69d981b52eece2983a5025
pan93412 <pan93412@gmail.com>
2024-05-28 03:00:28 +0800

    feat: initial telemetry support

--- src/iot_recognition_service/grpc_service.py
+++ src/iot_recognition_service/grpc_service.py
@@ -1,9 +1,8 @@
 import logging
-from tempfile import TemporaryFile
 from traceback import print_exception
-import cv2
 import grpc
-import numpy as np
+
+from opentelemetry import trace
 
 from iot_recognition_service.recognition import Recognizer
 from .protos.entityrecognitionpb_pb2 import RecognizeRequest, RecognizeResponse, Entity
@@ -12,30 +11,41 @@ from .protos.entityrecognitionpb_pb2_grpc import EntityRecognitionServicer
 class RecognitionService(EntityRecognitionServicer):
     """The gRPC service for recognizing objects in an image."""
 
+    logger = logging.getLogger(__name__)
+    tracer = trace.get_tracer(__name__)
+
     def __init__(self, recognizer: Recognizer):
         self.recognizer = recognizer
 
     def Recognize(self, request: RecognizeRequest, context: grpc.ServicerContext) -> RecognizeResponse:
-        try:
-            entities = self.recognizer.recognize_picture(request.image)
-        except ValueError as e:
-            print_exception(e)
-            context.abort(grpc.StatusCode.INVALID_ARGUMENT, str(e))
-        except RuntimeError as e:
-            print_exception(e)
-            context.abort(grpc.StatusCode.INTERNAL, str(e))
+        with self.tracer.start_as_current_span("RecognitionService/Recognize") as span:
 
-        return RecognizeResponse(
-            entities=(
-                Entity(
-                    label=entity.label,
-                    x1=entity.x1,
-                    x2=entity.x2,
-                    y1=entity.y1,
-                    y2=entity.y2,
-                    confidence=entity.confidence,
-                    image=entity.image,
+            span.add_event("recognize picture")
+            try:
+                entities = self.recognizer.recognize_picture(request.image)
+            except ValueError as e:
+                span.set_status(trace.StatusCode.ERROR, "User specifies a invalid argument")
+                span.record_exception(e)
+
+                context.abort(grpc.StatusCode.INVALID_ARGUMENT, f"Invalid argument: {e}")
+            except RuntimeError as e:
+                span.set_status(trace.StatusCode, )
+
+                print_exception(e)
+                context.abort(grpc.StatusCode.INTERNAL, str(e))
+            span.add_event("done recognize picture")
+
+            return RecognizeResponse(
+                entities=(
+                    Entity(
+                        label=entity.label,
+                        x1=entity.x1,
+                        x2=entity.x2,
+                        y1=entity.y1,
+                        y2=entity.y2,
+                        confidence=entity.confidence,
+                        image=entity.image,
+                    )
+                    for entity in entities
                 )
-                for entity in entities
             )
-        )
---
+++ src/iot_recognition_service/telemetry.py
@@ -0,0 +1,77 @@
+import logging
+import os
+from uuid import uuid4
+from opentelemetry.trace import set_tracer_provider
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    BatchSpanProcessor,
+)
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.sdk._logs import LoggerProvider
+from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogRecordProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+from opentelemetry.sdk.trace.export import SpanExporter
+from opentelemetry.sdk._logs.export import LogExporter
+
+
+
+resource = Resource(attributes={
+    ResourceAttributes.SERVICE_NAME: "iot-recognition-service",
+    ResourceAttributes.SERVICE_VERSION: "v0",
+    ResourceAttributes.SERVICE_INSTANCE_ID: uuid4().hex,
+}, schema_url=ResourceAttributes.SCHEMA_URL)
+
+def setup_telemetry() -> None:
+    """Setup the OpenTelemetry SDK for Baselime, and instrument the gRPC client."""
+
+    trace_provider = TracerProvider(resource=resource)
+    trace_provider.add_span_processor(
+        BatchSpanProcessor(create_trace_exporter())
+    )
+    set_tracer_provider(trace_provider)
+
+    logger_provider = LoggerProvider(resource=resource)
+    logger_provider.add_log_record_processor(
+       BatchLogRecordProcessor(create_log_exporter())
+    )
+    set_logger_provider(logger_provider)
+
+    GrpcInstrumentorClient().instrument()
+
+
+def create_trace_exporter() -> SpanExporter:
+    """Setup the OpenTelemetry OTLP Span Exporter for Baselime."""
+
+    baselime_api_key = os.getenv("BASELIME_API_KEY")
+    if not baselime_api_key:
+        logging.warn("BASELIME_API_KEY is not set. Using the stdout OTLP exporter.")
+        return ConsoleSpanExporter()
+
+    baselime_dataset = os.getenv("BASELIME_DATASET", "otel")
+
+    return OTLPSpanExporter(endpoint="otel.baselime.io/v1/traces", headers={
+        "x-api-key": baselime_api_key,
+        "x-baselime-dataset": baselime_dataset,
+    })
+
+
+def create_log_exporter() -> LogExporter:
+    """Setup the OpenTelemetry OTLP Log Exporter for Baselime."""
+
+    baselime_api_key = os.getenv("BASELIME_API_KEY")
+    if not baselime_api_key:
+        logging.warn("BASELIME_API_KEY is not set. Using the stdout OTLP exporter.")
+        return ConsoleLogExporter()
+
+    baselime_dataset = os.getenv("BASELIME_DATASET", "otel")
+
+    return OTLPLogExporter(endpoint="otel.baselime.io/v1/logs", headers={
+        "x-api-key": baselime_api_key,
+        "x-baselime-dataset": baselime_dataset,
+    })
+
