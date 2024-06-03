d4b09588758c46b19972789d5b966f5a763c4684
pan93412 <pan93412@gmail.com>
2024-06-03 15:24:01 +0800

    feat(telemetry): Allow customize environment

--- src/iot_recognition_service/telemetry.py
+++ src/iot_recognition_service/telemetry.py
@@ -27,7 +27,7 @@ resource = Resource(attributes={
     ResourceAttributes.SERVICE_NAMESPACE: "iot-monitor",
     ResourceAttributes.SERVICE_VERSION: "v0",
     ResourceAttributes.SERVICE_INSTANCE_ID: uuid4().hex,
-    ResourceAttributes.DEPLOYMENT_ENVIRONMENT: "development", # fixme: switch to production after online
+    ResourceAttributes.DEPLOYMENT_ENVIRONMENT: os.getenv("ENVIRONMENT", "development")
 }, schema_url=ResourceAttributes.SCHEMA_URL)
 
 
